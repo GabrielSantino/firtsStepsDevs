fc068e18444a582ad5757b131090389bee9980cf
Gabriel Santino <gabrielsantino.f@outlook.com>
2022-09-26 17:03:44 -0300

    JS Update

---
+++ fundamentos-js/objeto2.js
@@ -0,0 +1,13 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
+
+// pode criar várias instancias pela palavra new, através de funções definidas , e também a partir da classe(class)
+ 
